a88fff5ce31ee8503200950eb266e105cf9c49f6
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-10-08 16:13:18 +0500

    11
    1

--- src/components/diagramm/Diagramm.tsx
+++ src/components/diagramm/Diagramm.tsx
@@ -28,22 +28,39 @@ interface IDiagrammProps {
 }
 
 const Diagramm: FC<IDiagrammProps> = ({diagramData}) => {
-  const xCoordinate = diagramData.rows.map(row => row.x)
-  const yCoordinates = diagramData.rows.map(row => row.y)
+  const xCoordinate = Array.from(diagramData.rows.map(row => row.x))
+  const yCoordinates = Array.from(diagramData.rows.map(row => row.y))
   const options = {
     responsive: true,
+    interaction: {
+      mode: 'index' as const,
+      intersect: false,
+    },
+    stacked: false,
     plugins: {
-      legend: {
-        position: 'top' as const,
-      },
       title: {
         display: true,
         text: `график ${diagramData.id} таблицы`,
       },
     },
+    scales: {
+      y: {
+        type: 'linear' as const,
+        display: true,
+        position: 'left' as const,
+      },
+      y1: {
+        type: 'linear' as const,
+        display: true,
+        position: 'right' as const,
+        grid: {
+          drawOnChartArea: false,
+        },
+      },
+    },
   };
 
-  const labels = ['x', 'y']
+  const labels = xCoordinate
 
   const data = {
     labels,
